2c3733d21ceb85dfd224e67cd211dca030567a37
nishida.masayuki <nishida.masayuki@classmethod.jp>
2017-01-30 03:06:43 +0900

    healthcheck

--- routes/trace.js
+++ routes/trace.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var XRay = require('aws-xray-sdk');
 var http = require('http');
-http = XRay.captureHTTPs(_http);
+http = XRay.captureHTTPs(http);
 
 router.get('/', function (req, res, next) {
     http.get("http://13.112.92.206", function (result) {
